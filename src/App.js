aaeb61c37daa9c62d720faf2254f770ae4309de6
Arbias <uranademi9@gmail.com>
2024-12-07 13:02:52 +0100

    added the app to git

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <h1>Welcome to My Website</h1>
+        <p>This is a simple React app with a navbar.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
