0fd74cf57457b45a1d5d57d21723536df58df855
Arbias <uranademi9@gmail.com>
2024-12-08 17:38:14 +0100

    added the styling at the notfound page bcuz it was suspiciouslly deleted

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,8 +3,9 @@ import "../App.css";
 
 function NotFound() {
   return (
-    <div className="main">{/* To set the bg color */}
-      <h1 className="t404">404</h1>
+    <div className="main">
+      {/* To set the bg color */}
+      <h1 className="t404">! 404 !</h1>
       <h1 className="title">Page Not Found</h1>
       <p className="title">Oops! The page you're looking for does not exist.</p>
     </div>
