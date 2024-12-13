08524cbbbf5812d057eebd91432af4597741e8aa
Arbias <uranademi9@gmail.com>
2024-12-13 11:58:58 +0100

    fixed errors

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
 function Score(props) {
-    const name = localStorage.getItem("userName").toUpperCase() || "YOU"; 
+    const name = localStorage.getItem("userName");
+    const displayName = name ? name.toUpperCase() : "YOU";
 
-    return <div className='score'>
-        <div className="ai-score">
-            <h1>{props.aiScore}</h1>
-            <h2>AI</h2>
+    return (
+        <div className='score'>
+            <div className="ai-score">
+                <h1>{props.aiScore}</h1>
+                <h2>AI</h2>
+            </div>
+            <div className="human-score">
+                <h1>{props.humanScore}</h1>
+                <h2>{displayName}</h2>
+            </div>
         </div>
-        <div className="human-score">
-            <h1>{props.humanScore}</h1>
-            <h2>{name}</h2>
-        </div>
-    </div>;
+    );
 }
 
 export default Score;
